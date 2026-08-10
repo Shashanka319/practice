add36717c67ab4a69394d235004f1c9599adfd75
Shashank K <sharathshashank1986@gmail.com>
2026-08-10 23:57:08 +0530

    practice

---
+++ javapractice/src/main/java/com/javapractice/Runner.java
@@ -0,0 +1,10 @@
+package com.javapractice;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class Runner {
+    public static void main(String[] args) {
+        log.info("Welcome to java Practice World...");
+    }
+}
