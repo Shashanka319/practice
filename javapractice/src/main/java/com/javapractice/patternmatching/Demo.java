fc5555a321756ca158efea16cd92a1c926e8d82d
Shashank K <sharathshashank1986@gmail.com>
2026-08-11 23:05:18 +0530

    pattern matching

---
+++ javapractice/src/main/java/com/javapractice/patternmatching/Demo.java
@@ -0,0 +1,82 @@
+package com.javapractice.patternmatching;
+
+public class Demo {
+    public static void main(String[] args) {
+       //normalPattern();
+       //fullPattern();
+       //getNumbers();
+        //printStar();
+       //printNumbers();
+        getcount();
+    }
+    public static void normalPattern(){
+        System.out.println("Welcome to java Practice World...");
+        for(int i=1;i<=5;i++){
+            System.out.print("* ");
+        }
+    }
+    public static void fullPattern(){
+        System.out.println("full Pattern");
+        int i;
+        int j;
+        for( i = 1; i <=5; i++){
+            for(j=1;j<=5;j++){
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+    }
+    public static void getNumbers(){
+        int n=6;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print(j +" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void printStar(){
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i==1|| i==n || j==1 || j==n){
+                System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void printNumbers(){
+        System.out.println();
+        int n = 9;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i==1 || i==n || j==1 || j==n){
+                    System.out.print(" "+j);
+                }else {
+                    System.out.print(" 0");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void getcount(){
+        int n=5;
+        int count =1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(count <10){
+                    System.out.print("0");
+                }
+                System.out.print(count+" ");
+                count++;
+            }
+            System.out.println();
+        }
+    }
+
+
+}
