31b0e7bbe862b09983e722c420ce163af9ea5868
Shashank K <sharathshashank1986@gmail.com>
2026-08-18 00:17:44 +0530

    Ternory Operators

--- javapractice/src/main/java/com/javapractice/casting/TypeCasting.java
+++ javapractice/src/main/java/com/javapractice/casting/TypeCasting.java
@@ -2,8 +2,9 @@ package com.javapractice.casting;
 
 public class TypeCasting {
     public static void main(String[] args) {
-      getImplicitly();
-      getExplicitly();
+      //getImplicitly();
+      //getExplicitly();
+      getPattern();
     }
    //implicitly
     public static void getImplicitly() {
@@ -18,6 +19,22 @@ public class TypeCasting {
         int b = (int)a;
         System.out.println(a);
         System.out.println(b);
+    }
+    public static void getPattern(){
+        char ch = 'A';
+        for(int i=1;i<=5;i++){
+
+            for(int j=1;j<=6;j++){
+                System.out.print(ch+" ");
+                //ch =(char) (ch+1);
+                //ch++;
+                ch +=1;
+                if(ch>'Z'){
+                    break;
+                }
+            }
+            System.out.println(" ");
+        }
     }
 
 }
